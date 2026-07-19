e6d88bd38bb251fc0951c371534d5ede76729190
N.Tsubasa <n.tsubasa@WIZ24PN005.local>
2026-07-20 01:21:49 +0900

    Add customer harassment policy footer link

---
+++ theme/touroku-s/footer.php
@@ -0,0 +1,71 @@
+<?php if ( is_active_sidebar( 'footer-upper-widget-1' ) ) : ?>
+<div class="section sectionBox siteContent_after">
+    <div class="container ">
+        <div class="row ">
+            <div class="col-md-12 ">
+            <?php dynamic_sidebar( 'footer-upper-widget-1' ); ?>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+<?php do_action('lightning_footer_before'); ?>
+
+<footer class="section siteFooter">
+	<div class="business-hours">
+		<div class="business-hours__item container">
+			<p>営業日：月曜日〜金曜日　土日祝休み</p>
+			<p>営業時間：8：00〜17：00</p>
+		</div>
+	</div>
+    <div class="footerMenu">
+       <div class="container">
+            <?php wp_nav_menu( array(
+                'theme_location'    => 'Footer',
+                'container'         => 'nav',
+                'items_wrap'        => '<ul id="%1$s" class="%2$s nav">%3$s</ul>',
+                'fallback_cb'       => '',
+                'depth'             => 1
+            ) ); ?>
+        </div>
+    </div>
+    <div class="container sectionBox">
+        <div class="row ">
+            <?php
+            // Area setting
+            $footer_widget_area = array(
+                // Use 1 widget area
+                1 => array('class' => 'col-md-12'),
+                // Use 2 widget area
+                2 => array('class' => 'col-md-6'),
+                // Use 3 widget area
+                3 => array('class' => 'col-md-4'),
+                // Use 4 widget area
+                4 => array('class' => 'col-md-3'),
+                );
+
+            $footer_widget_area_count = 3;
+            $footer_widget_area_count = apply_filters( 'lightning_footer_widget_area_count', $footer_widget_area_count );
+
+            // Print widget area
+            for ( $i = 1; $i <= $footer_widget_area_count; ) {
+                echo '<div class="'.$footer_widget_area[$footer_widget_area_count]['class'].'">';
+                    if ( is_active_sidebar( 'footer-widget-'.$i ) ) dynamic_sidebar( 'footer-widget-'.$i );
+                    if ( 2 === $i ) {
+                        echo '<a href="/contact/%e3%82%ab%e3%82%b9%e3%82%bf%e3%83%9e%e3%83%bc%e3%83%8f%e3%83%a9%e3%82%b9%e3%83%a1%e3%83%b3%e3%83%88%e3%81%ab%e5%af%be%e3%81%99%e3%82%8b%e5%9f%ba%e6%9c%ac%e6%96%b9%e9%87%9d/" class="customer_policy_link_footer">カスタマーハラスメントに対する基本方針</a>';
+                    }
+                echo '</div>';
+                $i++;
+            }
+            ?>
+        </div>
+    </div>
+    <div class="container sectionBox copySection text-center">
+          <p>Copyright &copy; 行政書士法人 登録サポート All Rights Reserved. </p>
+    </div>
+</footer>
+<?php do_action('lightning_footer_after'); ?>
+<?php wp_footer();?>
+</body>
+</html>
